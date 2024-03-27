65a2abc8862aa677ded89fe30cc4525ce597b748
Guga <gugakobakhidze18@gmail.com>
2024-03-27 06:21:01 +0400

    changing logiv

--- src/components/meme/MemeBox.tsx
+++ src/components/meme/MemeBox.tsx
@@ -4,26 +4,49 @@ import memeData from "../../data/MemeData";
 import { Box } from "@mui/system";
 import { MemeBoxStyle } from "./MemeStyle";
 
+interface MemeProps {
+  topText: string;
+  bottomText: string;
+  imageUrl: string;
+}
+
+interface MemeDataProps {
+  id: string;
+  name: string;
+  url: string;
+  width: number;
+  height: number;
+  box_count: number;
+}
+
 const MemeBox = () => {
-  const [memeImg, setMemeImg] = useState<string>("");
+  const [allMemes] = useState<MemeDataProps[]>(memeData);
+  const [meme, setMeme] = useState<MemeProps>({
+    topText: "",
+    bottomText: "",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  console.log(meme);
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    const memeArr = memeData;
+    const memeArr = allMemes;
     const randomNumber = Math.floor(Math.random() * memeArr.length);
     const url = memeArr[randomNumber].url;
 
-    setMemeImg(url);
+    setMeme((prev) => ({
+      ...prev,
+      imageUrl: url,
+    }));
   };
-
-  console.log(memeImg);
 
   return (
     <Box sx={{ ...MemeBoxStyle }}>
       <Meme onFormSubmit={onSubmit} />
       <Box mt={2} sx={{ img: { width: "100%", borderRadius: 2 } }}>
-        {memeImg && <img src={memeImg} alt="MemeImg" />}
+        <img src={meme.imageUrl} alt="MemeImg" />
       </Box>
     </Box>
   );
